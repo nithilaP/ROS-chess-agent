df7bc033e4464f1d4e737f76c340dcd491f8ffb2
Ronan Loberg <103848983+Ronan-Loberg@users.noreply.github.com>
2023-12-07 22:49:07 -0800

    Add files via upload

---
+++ Final_Arduino_Code.ino
@@ -0,0 +1,93 @@
+// Define pin connections & motor's steps per revolution
+const int xDIR = 2;
+const int xSTEP = 3;
+const int yDIR = 4;
+const int ySTEP = 5;
+const int zDIR = 6;
+const int zSTEP= 7;
+const int stepsPerRevolution = 200;
+
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(xDIR, OUTPUT);
+  pinMode(xSTEP, OUTPUT);
+  pinMode(yDIR, OUTPUT);
+  pinMode(ySTEP, OUTPUT);
+  pinMode(zDIR, OUTPUT);
+  pinMode(zSTEP, OUTPUT);
+  Serial.begin(9600);
+}
+  float POS[2] = {0,0};
+  float userdata[2] = {0,0};
+  bool pincher = false; //true means actively pinching
+  
+void loop()
+{
+  if (Serial.available() > 0) {
+    for (int i=0; i<3;i++) {
+      userdata[i] = Serial.read();
+      
+    }
+    
+    Serial.println("Sending to coords: ");
+    Serial.println(userdata[1], userdata[2]);
+  }
+  float xdis = userdata[1] - POS[1];
+  float ydis = userdata[1] - POS[1]; //distance in inches needed to travel
+  // one revolution = 1.88" TEST, thus 200 steps = 1.88"
+  float xSteps = 200*xdis/1.88;
+  float ySteps = 200*ydis/1.88;
+  
+  // Set motor direction
+  if (xdis > 0) {
+    digitalWrite(xDIR, HIGH);
+  } else {
+    digitalWrite(xDIR, LOW);
+  }
+  if (ydis > 0) {
+    digitalWrite(yDIR, HIGH);
+  } else {
+    digitalWrite(yDIR, LOW);
+  }  
+
+  // Spin motors slowly
+  for(int x = 0; x < xSteps; x++)
+  {
+    digitalWrite(xSTEP, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(xSTEP, LOW);
+    delayMicroseconds(2000);
+  }
+  for(int y = 0; y < ySteps; y++)
+  {
+    digitalWrite(ySTEP, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(ySTEP, LOW);
+    delayMicroseconds(2000);
+  }
+  
+  delay(1000); // Wait a second
+
+  //Now use pincher
+  if (pincher) {
+    digitalWrite(zDIR,HIGH);
+  }else {
+    digitalWrite(zDIR,LOW);
+  }
+    for(int z = 0; z < 100; z++)//only needs a half rotation (stepsPerRevolution / 2)
+  {
+    digitalWrite(zSTEP, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(zSTEP, LOW);
+    delayMicroseconds(2000);
+  }
+  delay(1000);
+  
+  //reset variables
+  pincher = not pincher;
+  POS[1] = userdata[1];
+  POS[2] = userdata[2];
+  Serial.println("move complete"); //Sanity check
+  
+}
