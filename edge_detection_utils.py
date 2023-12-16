5eb80f36d4c8fdd496029fefce3e2425994b4ef6
Gurvir11 <gurvirkooner11@gmail.com>
2023-12-16 14:40:56 -0800

    forgot to commit final project - doing it now

--- edge_detection_utils.py
+++ edge_detection_utils.py
@@ -1,6 +1,7 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+from sklearn.cluster import KMeans
 
 LETTERS =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
@@ -17,29 +18,8 @@ def convert_to_chess_loc(i, j):
 def convert_to_2d(chess_loc):
     return (LETTERS.index(chess_loc[0]), 8 - int(chess_loc[1]))
 
-def cyclic_intersection_pts(pts):
-    if pts.shape[0] != 4:
-        return None
-
-    # Calculate the center
-    center = np.mean(pts, axis=0)
-
-    # Sort the points in clockwise
-    cyclic_pts = [
-        # Top-left
-        pts[np.where(np.logical_and(pts[:, 0] < center[0], pts[:, 1] < center[1]))[0][0], :],
-        # Top-right
-        pts[np.where(np.logical_and(pts[:, 0] > center[0], pts[:, 1] < center[1]))[0][0], :],
-        # Bottom-Right
-        pts[np.where(np.logical_and(pts[:, 0] > center[0], pts[:, 1] > center[1]))[0][0], :],
-        # Bottom-Left
-        pts[np.where(np.logical_and(pts[:, 0] < center[0], pts[:, 1] > center[1]))[0][0], :]
-    ]
-
-    return np.array(cyclic_pts)
-
-def drawHoughLines(image, lines, output):
-    out = image.copy()
+def display_hough_lines(image, lines, output):
+    hough_lines = image.copy()
     for line in lines:
         rho, theta = line[0]
         a = np.cos(theta)
@@ -50,63 +30,58 @@ def drawHoughLines(image, lines, output):
         y1 = int(y0 + 10000 * (a))
         x2 = int(x0 - 10000 * (-b))
         y2 = int(y0 - 10000 * (a))
-        cv2.line(out, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    cv2.imwrite(output, out)
+        cv2.line(hough_lines, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.imwrite(output, hough_lines)
 
-def intersection(m1: float, b1: float, m2: float, b2: float):
-    # Consider y to be equal and solve for x
-    # Solve:
-    #   m1 * x + b1 = m2 * x + b2
-    if b2 is np.nan or b1 is np.nan or m1 is np.nan or m2 is np.nan or m1 - m2 == 0:# 0.0000000001:
+def get_intersect_pt(m1, b1, m2, b2):
+    if b2 is np.nan or b1 is np.nan or m1 is np.nan or m2 is np.nan or m1 - m2 == 0:
         return None, None
     x = (b2 - b1) / (m1 - m2)
-    # Use the value of x to calculate y
     y = m1 * x + b1
 
     return int(round(x)), int(round(y))
 
-def hough_lines_intersection(lines: np.array, image_shape: tuple):
-    """
-    Returns the intersection points that lie on the image
-    for all combinations of the lines
-    """
-    if len(lines.shape) == 3 and \
-            lines.shape[1] == 1 and lines.shape[2] == 2:
+def get_intersect_pts(lines, image):
+    if len(lines.shape) == 3 and lines.shape[1] == 1 and lines.shape[2] == 2:
         lines = np.squeeze(lines)
-    lines_count = len(lines)
+    num_lines = len(lines)
     intersect_pts = []
-    for i in range(lines_count - 1):
-        for j in range(i + 1, lines_count):
-            # print(lines[i])
-            m1, b1 = polar2cartesian(lines[i][0], lines[i][1], True)
-            m2, b2 = polar2cartesian(lines[j][0], lines[j][1], True)
-            x, y = intersection(m1, b1, m2, b2)
-            # print(x, y)
-            if x is not None and y is not None and point_on_image(x, y, image_shape):
+    for i in range(num_lines - 1):
+        for j in range(i + 1, num_lines):
+            i_rho, i_theta = lines[i]
+            j_rho, j_theta = lines[j]
+            m1, b1 = get_slope_intersect(i_rho, i_theta)
+            m2, b2 = get_slope_intersect(j_rho, j_theta)
+            x, y = get_intersect_pt(m1, b1, m2, b2)
+            if x is not None and y is not None and is_on_image(x, y, image.shape):
                 intersect_pts.append([x, y])
-                # print("appended")
-    return np.array(intersect_pts, dtype=int)
+    intersect_pts = np.array(intersect_pts, dtype=int)
+
+    if intersect_pts.shape[0] != 4:
+        return None
+
+    x_center, y_center = np.mean(intersect_pts, axis=0)
+
+    organized_intersect_pts = [0] * 4
+
+    organized_intersect_pts[0] = intersect_pts[np.where(np.logical_and(intersect_pts[:, 0] < x_center,
+                                                                        intersect_pts[:, 1] < y_center))[0][0], :]
+    organized_intersect_pts[1] = intersect_pts[np.where(np.logical_and(intersect_pts[:, 0] > x_center,
+                                                                        intersect_pts[:, 1] < y_center))[0][0], :]
+    organized_intersect_pts[2] = intersect_pts[np.where(np.logical_and(intersect_pts[:, 0] > x_center,
+                                                                        intersect_pts[:, 1] > y_center))[0][0], :]
+    organized_intersect_pts[3] = intersect_pts[np.where(np.logical_and(intersect_pts[:, 0] < x_center,
+                                                                        intersect_pts[:, 1] > y_center))[0][0], :]
+
+    return np.array(organized_intersect_pts)
 
 
-def polar2cartesian(rho: float, theta_rad: float, rotate90: bool = False):
-    """
-    Converts line equation from polar to cartesian coordinates
-    Args:
-        rho: input line rho
-        theta_rad: input line theta
-        rotate90: output line perpendicular to the input line
-    Returns:
-        m: slope of the line
-           For horizontal line: m = 0
-           For vertical line: m = np.nan
-        b: intercept when x=0
-    """
+def get_slope_intersect(rho, theta_rad):
     x = np.cos(theta_rad) * rho
     y = np.sin(theta_rad) * rho
     m = np.nan
     if not np.isclose(x, 0.0):
         m = y / x
-    if rotate90:
         if m is np.nan:
             m = 0.0
         elif np.isclose(m, 0.0):
@@ -119,11 +94,9 @@ def polar2cartesian(rho: float, theta_rad: float, rotate90: bool = False):
     
     return m, b
 
-def point_on_image(x: int, y: int, image_shape: tuple):
-    """
-    Returns true is x and y are on the image
-    """
-    return 0 <= y < image_shape[0] and 0 <= x < image_shape[1]
+def is_on_image(x, y, image_shape):
+    img_y, img_x = image_shape
+    return 0 <= y < img_y and 0 <= x < img_x
 
 def piece_detection(patch_image, im_dst):
     global MIN_BLUE_HSV_MASK
@@ -139,10 +112,7 @@ def piece_detection(patch_image, im_dst):
     blanks_green = np.ones_like(im_dst) * 255
     blanks_blue = np.ones_like(im_dst) * 255
 
-    # convert to white and black frame
     green = cv2.bitwise_and(blanks_green, blanks_green, mask=mask1)
-    # black_out2 = cv2.bitwise_and(blanks, blanks, mask=mask2)
-    # black_out = cv2.bitwise_or(black_out1, black_out2)
     green_outline = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
     blue = cv2.bitwise_and(blanks_blue, blanks_blue, mask=mask2)
     blue_outline = cv2.cvtColor(blue, cv2.COLOR_BGR2GRAY)
@@ -160,3 +130,17 @@ def piece_detection(patch_image, im_dst):
         return "white"
     return None
 
+
+def get_hough_lines(contour_outline, angle_cutoff):
+    hough_lines = cv2.HoughLines(contour_outline, 1, np.pi / 180, 300, min_theta=1e-9)
+    hough_lines_squeeze = np.squeeze(hough_lines)
+    cutoff = np.pi/angle_cutoff
+    hough_lines = hough_lines[(hough_lines_squeeze[:, 1] < cutoff) | 
+                              (hough_lines_squeeze[:, 1] > (angle_cutoff-1)*cutoff) | 
+                              ((hough_lines_squeeze[:, 1] > (angle_cutoff-1)*cutoff/2) & 
+                               (hough_lines_squeeze[:, 1] < (angle_cutoff+1)*cutoff/2))]
+    hough_lines[hough_lines[:, 0, 1] > (angle_cutoff-1)*np.pi/cutoff, 0, 0] *= -1
+    hough_lines[hough_lines[:, 0, 1] > (angle_cutoff-1)*np.pi/cutoff, 0, 1] -= np.pi
+    kmeans = KMeans(n_clusters=4, n_init=10, max_iter=300).fit(hough_lines[:, 0, :])
+    hough_lines =  np.expand_dims(kmeans.cluster_centers_, axis=1)
+    return hough_lines
--- main.py
+++ main.py
@@ -5,24 +5,27 @@ from sklearn.cluster import KMeans
 from edge_detection_utils import *
 from chess_engine import *
 import chess
+import serial
+from solver_to_gantry import *
 
-# RED HSV Value Calibration
 MIN_RED_HSV_MASK1 = (0, 70, 50)
 MAX_RED_HSV_MASK1 = (10, 255, 255)
 MIN_RED_HSV_MASK2 = (170, 70, 50)
 MAX_RED_HSV_MASK2 = (180, 255, 255)
 
-LINE_ANGLE_CUTOFF = 9
+ANGLE_CUTOFF = 9
 WIDTH_SQUARE = 8
 HEIGHT_SQUARE = 8
-MULTIPLIER_LENGTH = 100
-INTERSECT_PTS_LAST = None
+MULTIPLIER = 100
+PREV_INTERSECT_PTS = None
 
-OFFSET = int(0.5 * MULTIPLIER_LENGTH) # 0.6
-SQUARE_SIZE = int(0.5 * MULTIPLIER_LENGTH) # 0.3
-CIRCLE_PIXELS = int(0.93 * MULTIPLIER_LENGTH)
+OFFSET = int(0.5 * MULTIPLIER)
+SQUARE_SIZE = int(0.5 * MULTIPLIER)
+BOX_PIXELS = int(0.93 * MULTIPLIER)
 
-BOARD_DISTANCE = 0
+
+# Definition for Arduino
+arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1) 
 
 def video_capture():
 
@@ -31,17 +34,15 @@ def video_capture():
     global MIN_RED_HSV_MASK2 
     global MAX_RED_HSV_MASK2 
 
-    global LINE_ANGLE_CUTOFF
+    global ANGLE_CUTOFF
     global WIDTH_SQUARE
     global HEIGHT_SQUARE
-    global MULTIPLIER_LENGTH
-    global INTERSECT_PTS_LAST
+    global MULTIPLIER
+    global PREV_INTERSECT_PTS
 
     global OFFSET
     global SQUARE_SIZE
-    global CIRCLE_PIXELS
-
-    global BOARD_DISTANCE
+    global BOX_PIXELS
 
     # Pipeline to get the data stream from the camera -> configuring camera
     pipeline = rs.pipeline()
@@ -77,29 +78,22 @@ def video_capture():
             if not depth_frame or not color_frame:
                 continue
 
-            depth_image = np.asanyarray(depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
 
             hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
-            # Get HSV values for tuning
-            # mean_center_hsv = np.mean(hsv[len(hsv)//2], axis=0)
-            # print('Current Mean Value:', mean_center_hsv)
-            # mask the red color of the board -> https://stackoverflow.com/questions/32522989/opencv-better-detection-of-red-color
             mask1 = cv2.inRange(hsv, MIN_RED_HSV_MASK1, MAX_RED_HSV_MASK1)
             mask2 = cv2.inRange(hsv, MIN_RED_HSV_MASK2, MAX_RED_HSV_MASK2)
             blanks = np.ones_like(color_image) * 255
 
-            # convert to white and black frame
             black_out1 = cv2.bitwise_and(blanks, blanks, mask=mask1)
             black_out2 = cv2.bitwise_and(blanks, blanks, mask=mask2)
             black_out = cv2.bitwise_or(black_out1, black_out2)
             black_white = cv2.cvtColor(black_out, cv2.COLOR_BGR2GRAY)
 
-            # contours
             contours, hierarchy = cv2.findContours(black_white, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            try: # does following code when it sees red in the frame
+            try: 
                 contour_idx = np.argmax([cv2.contourArea(cont) for cont in contours])
                 cv2.drawContours(color_image, contours, contour_idx, (255, 255, 255), 3)
                 contour_image = np.zeros_like(color_image)
@@ -108,69 +102,59 @@ def video_capture():
             except ValueError:
                 pass
 
-            # cv2.namedWindow("RealSense", cv2.WINDOW_AUTOSIZE)
             cv2.imshow("Color", color_image)
             keystroke = cv2.waitKey(1)
             if keystroke == ord('q'):
                 break
             if keystroke == ord(" "):
                 cv2.imwrite("contours_color.png", color_image)
-                print("TOOK A SNAPSHOT")
                 cv2.imwrite("contour_outline_img.png", contour_outline_image)
             
-                # Hough Lines
                 try:
-                    # blank = blank[CROP:-CROP, CROP:-CROP] # Can crop the contour outline image
-                    polar_lines = cv2.HoughLines(contour_outline_image, 1, np.pi / 180, 300, min_theta=1e-9)
-                    polar_lines_squeeze = np.squeeze(polar_lines)
+                    hough_lines = cv2.HoughLines(contour_outline_image, 1, np.pi / 180, 300, min_theta=1e-9)
+                    hough_lines_squeeze = np.squeeze(hough_lines)
 
-                    LINE_ANGLE_CUTOFF = 9
-                    polar_lines = polar_lines[(polar_lines_squeeze[:, 1] < np.pi/LINE_ANGLE_CUTOFF) | (polar_lines_squeeze[:, 1] > (LINE_ANGLE_CUTOFF-1)*np.pi/LINE_ANGLE_CUTOFF) | ((polar_lines_squeeze[:, 1] > (LINE_ANGLE_CUTOFF-1)*np.pi/LINE_ANGLE_CUTOFF/2) &  (polar_lines_squeeze[:, 1] < (LINE_ANGLE_CUTOFF+1)*np.pi/LINE_ANGLE_CUTOFF/2))]
-                    polar_lines[polar_lines[:, 0, 1] > (LINE_ANGLE_CUTOFF-1)*np.pi/LINE_ANGLE_CUTOFF, 0, 0] *= -1
-                    polar_lines[polar_lines[:, 0, 1] > (LINE_ANGLE_CUTOFF-1)*np.pi/LINE_ANGLE_CUTOFF, 0, 1] -= np.pi
+                    hough_lines = hough_lines[(polar_lines_squeeze[:, 1] < np.pi/ANGLE_CUTOFF) | (polar_lines_squeeze[:, 1] > (ANGLE_CUTOFF-1)*np.pi/ANGLE_CUTOFF) | ((polar_lines_squeeze[:, 1] > (ANGLE_CUTOFF-1)*np.pi/ANGLE_CUTOFF/2) & (polar_lines_squeeze[:, 1] < (ANGLE_CUTOFF+1)*np.pi/ANGLE_CUTOFF/2))]
+                    hough_lines[hough_lines[:, 0, 1] > (ANGLE_CUTOFF-1)*np.pi/ANGLE_CUTOFF, 0, 0] *= -1
+                    hough_lines[hough_lines[:, 0, 1] > (ANGLE_CUTOFF-1)*np.pi/ANGLE_CUTOFF, 0, 1] -= np.pi
                 
-                    kmeans = KMeans(n_clusters=4, n_init=10, max_iter=300).fit(polar_lines[:, 0, :])
-                    polar_lines =  np.expand_dims(kmeans.cluster_centers_, axis=1)
-                    drawHoughLines(color_image, polar_lines, 'hough_lines.png')
+                    kmeans = KMeans(n_clusters=4, n_init=10, max_iter=300).fit(hough_lines[:, 0, :])
+                    hough_lines =  np.expand_dims(kmeans.cluster_centers_, axis=1)
+                    display_hough_lines(color_image, hough_lines, 'hough_lines.png')
 
-                    # Detect the intersection points
-                    intersect_pts = hough_lines_intersection(polar_lines, contour_image.shape)
-                    intersect_pts = cyclic_intersection_pts(intersect_pts)
+                    intersect_pts = get_intersect_pts(hough_lines, contour_image)
                     if intersect_pts is None:
-                        raise Exception("Intersections not found") 
+                        raise Exception("Intersection points not found") 
 
-                    INTERSECT_PTS_LAST = intersect_pts
+                    PREV_INTERSECT_PTS = intersect_pts
                     for pt in intersect_pts:
-                        cv2.circle(color_image, (pt[0], pt[1]), radius=5, thickness=-1, color=(0, 255, 0))
+                        x, y = pt
+                        cv2.circle(color_image, (x, y), radius=5, thickness=-1, color=(0, 255, 0))
                     cv2.imwrite('intersections.png',color_image) 
                     
                 except Exception as e: 
-                    print("Homography failed...")
+                    print("Failed to Properly Parse Board")
                     print(e)
-                    intersect_pts = INTERSECT_PTS_LAST
+                    intersect_pts = PREV_INTERSECT_PTS
                     if intersect_pts is None:
                         continue
 
-                h, status = cv2.findHomography(intersect_pts, np.array([[0, 0], [WIDTH_SQUARE*MULTIPLIER_LENGTH, 0], [WIDTH_SQUARE*MULTIPLIER_LENGTH, HEIGHT_SQUARE*MULTIPLIER_LENGTH], [0, HEIGHT_SQUARE*MULTIPLIER_LENGTH]]))
-                im_dst = cv2.warpPerspective(color_image, h, (WIDTH_SQUARE*MULTIPLIER_LENGTH,  HEIGHT_SQUARE*MULTIPLIER_LENGTH))
+                h, status = cv2.findHomography(intersect_pts, np.array([[0, 0], [WIDTH_SQUARE*MULTIPLIER, 0], [WIDTH_SQUARE*MULTIPLIER, HEIGHT_SQUARE*MULTIPLIER], [0, HEIGHT_SQUARE*MULTIPLIER]]))
+                im_dst = cv2.warpPerspective(color_image, h, (WIDTH_SQUARE*MULTIPLIER,  HEIGHT_SQUARE*MULTIPLIER))
 
 
                 cv2.imwrite("cleared.png", im_dst)
 
 
-                # Image Frame Processing -> Get & Analyze Gamestate
                 current_state = np.array([[None] * WIDTH_SQUARE] * HEIGHT_SQUARE)            
                 for i in range(WIDTH_SQUARE):
                     for j in range(HEIGHT_SQUARE):
-                        # cv2.circle(output[0], (65 + i * 93, 65 + 93 * j), radius=5, thickness=-1, color=(0, 255, 0))
-                        # cv2.circle(output[0], (70 + i * 93, 70 + 93 * j), radius=5, thickness=-1, color=(0, 255, 0))
-                        min_x = OFFSET + i * CIRCLE_PIXELS
+                        min_x = OFFSET + i * BOX_PIXELS
                         max_x = min_x + SQUARE_SIZE
-                        min_y = OFFSET + j * CIRCLE_PIXELS
+                        min_y = OFFSET + j * BOX_PIXELS
                         max_y = min_y + SQUARE_SIZE
                         cv2.rectangle(im_dst, (min_x, min_y), (max_x, max_y), color=(0, 255, 0), thickness=2)
                         patch = im_dst[min_y:max_y, min_x:max_x]
-                        # print(patch.shape)
 
                         chess_loc = convert_to_chess_loc(i, j)
                         current_detection = piece_detection(patch, im_dst)
@@ -178,7 +162,6 @@ def video_capture():
                         if current_detection != None:
                             print(f"{current_detection} piece detected at", chess_loc)
                         
-                        # Using cv2.putText() method
                         im_dst = cv2.putText(im_dst, chess_loc, (int(max_x - SQUARE_SIZE/ 2), int(max_y - SQUARE_SIZE/ 2)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=(255, 0, 0), thickness=1)
                         
                 cv2.imwrite("outlined_gamestate.png", im_dst)
@@ -191,7 +174,10 @@ def video_capture():
                 board.push_san(player_move)
                 robot_move = get_best_move(board, "black")
                 print("ROBOT MOVE:", robot_move)
+
                 # send commands to Arduino function -> wait for response
+                arduino.write(bytes(x, 'utf-8')) 
+
                 board.push_san(robot_move)
                 last_state_mask = current_state_mask
                 last_state = current_state
